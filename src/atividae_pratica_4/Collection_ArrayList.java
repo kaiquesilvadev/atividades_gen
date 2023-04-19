e5d98a19576051ad6ae91ca81319616c4845c785
kaiquesilvadev <j.kaique1570@gmail.com>
2023-04-19 17:43:30 -0300

    atividade collection

---
+++ src/atividae_pratica_4/Collection_ArrayList.java
@@ -0,0 +1,26 @@
+package atividae_pratica_4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Collection_ArrayList {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		int x;
+		
+		ArrayList<String> cores = new ArrayList();
+	
+		for(x =0 ;x<5;x++) {
+		System.out.println("entre com uma cor");
+		String cor = leia.nextLine();
+		cores.add(cor);
+		}
+		System.out.println("cores adicionadas ");
+		
+			System.out.println(cores);
+		
+	leia.close();
+	}
+
+}
